52b8a839e47324ca92ab3c0f53eb96b90e58181e
Antony Wong <aeq2005@163.com>
2016-12-01 15:27:08 +0800

    add Protocol：Device

--- LightingTo3.5/LightingTo3.5/CLEADevice.swift
+++ LightingTo3.5/LightingTo3.5/CLEADevice.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+// 协议(是CLEAProtocol用到的,用于回调什么?)
+protocol Device {
+    func setRegister(register: UInt8, value: UInt8, page: UInt8)
+    func setStatus(status: [UInt8], count: Int)
+}
+
 class CLEADevice: NSObject {
 
 }
