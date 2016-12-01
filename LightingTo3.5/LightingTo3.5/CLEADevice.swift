c247dce589c4f540e52bba2e6b90dcbba1d2a1fe
Antony Wong <aeq2005@163.com>
2016-12-01 11:54:42 +0800

    creat new class: CLEADevice

---
+++ LightingTo3.5/LightingTo3.5/CLEADevice.swift
@@ -0,0 +1,13 @@
+//
+//  CLEADevice.swift
+//  LightingTo3.5
+//
+//  Created by SPK_Antony on 01/12/2016.
+//  Copyright © 2016 Spark Technology Inc. All rights reserved.
+//
+
+import UIKit
+
+class CLEADevice: NSObject {
+
+}
