cf41881555e310710f95c4d2e419f54db105c139
Antony Wong <aeq2005@163.com>
2016-12-05 14:45:42 +0800

    implement Alert and Action

--- LightingTo3.5/LightingTo3.5/DeviceViewController.swift
+++ LightingTo3.5/LightingTo3.5/DeviceViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class DeviceViewController: UITableViewController {
+    
+    let notFoundAlert = UIAlertController(title: nil, message: nil, preferredStyle:UIAlertControllerStyle.alert)
+    
+    let updateAlert = UIAlertController(title: nil, message: nil, preferredStyle:UIAlertControllerStyle.alert)
+    
+    let colseAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: {
+        (act: UIAlertAction)in
+        // do nothing
+    })
+    
+    let updteAction = UIAlertAction.init(title: "Update", style: UIAlertActionStyle.default, handler: {
+        // 这个就是Swift中的闭包?
+        (act: UIAlertAction)in
+        CLEADevice.shared().updateFw()
+    })
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 通告中的闭包?(OC中的Block?)
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.EAAccessoryDidConnect, object: nil, queue: nil, using: {
+            (notification: Notification)in
+            log(message: "EAAccessoryDidConnect", obj: self)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
