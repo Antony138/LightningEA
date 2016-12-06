40149778d8358aa12e2ccf89894b0911b1d6ca83
Antony Wong <aeq2005@163.com>
2016-12-06 09:05:55 +0800

    connect outlet properties

--- LightingTo3.5/LightingTo3.5/DeviceViewController.swift
+++ LightingTo3.5/LightingTo3.5/DeviceViewController.swift
@@ -24,7 +24,29 @@ class DeviceViewController: UITableViewController {
         (act: UIAlertAction)in
         CLEADevice.shared().updateFw()
     })
+    
+    
+    @IBOutlet weak var hwName: UILabel!
+    
+    @IBOutlet weak var hwManufacturer: UILabel!
+    
+    @IBOutlet weak var hwModel: UILabel!
+    
+    @IBOutlet weak var hwSerialN: UILabel!
+    
+    @IBOutlet weak var hwSerialImageView: UIImageView!
+    
+    @IBOutlet weak var hwRevision: UILabel!
+    
+    @IBOutlet weak var fwVersion: UILabel!
+    
+    @IBOutlet weak var loadedFWVersion: UILabel!
 
+    @IBOutlet weak var fwStatus: UILabel!
+    
+    @IBOutlet weak var updateFwBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
